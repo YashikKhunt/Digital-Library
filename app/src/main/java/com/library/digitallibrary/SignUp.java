19d8215261959a0159efdb51bd00e5dd66027ba5
Khunt Yashik <yashikkhunt2002@gmail.com>
2020-09-22 09:54:03 +0530

    Login And SigUp Ui

--- app/src/main/java/com/library/digitallibrary/SignUp.java
+++ app/src/main/java/com/library/digitallibrary/SignUp.java
@@ -5,34 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
     //variables
-    TextInputLayout regUsername,regEmail,regPassword;
-    Button regSignUp,regToLoginBtn;
+    TextInputLayout regUsername, regEmail, regPassword;
+    Button regSignUp, regToLoginBtn;
 
-    DatabaseReference databaseReference;
-    FirebaseAuth firebaseAuth;
+    // Write a message to the database
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
+    String databaseUrl = "https://digital-library-5dad1.firebaseio.com/";
+    //authenticate email and password with firebase authentication
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
         //hooks to all xml ID's
@@ -42,52 +47,125 @@ public class SignUp extends AppCompatActivity {
         regSignUp = findViewById(R.id.reg_sign_up);
         regToLoginBtn = findViewById(R.id.reg_signUp_to_LogIn);
 
-        //save data into the firebase on signUp button click and Authentication using Email and password
-        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
-        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
+        //save data into the firebase on signUp button click and Authentication using Email and password
         regSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username = regUsername.getEditText().toString();
-                final String email = regEmail.getEditText().toString();
-                String password = regPassword.getEditText().toString();
-
-                if(TextUtils.isEmpty(username)){
-                    Toast.makeText(SignUp.this, "Please Enter Username", Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(SignUp.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(SignUp.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                //cheak validation
+                if(!registerUser() | !registerEmail() | !registerPassword()){
+                    return;
                 }
 
-                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                //connection
+                database = FirebaseDatabase.getInstance(databaseUrl);
+                myRef = database.getReference("users");
+
+
+
+                //get all the values
+                final String username = regUsername.getEditText().getText().toString();
+                final String email = regEmail.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
 
-                                    UserHelperClass information = new UserHelperClass(username, email);
 
-                                    FirebaseDatabase.getInstance().getReference("Users")
-                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                            .setValue(information).addOnCompleteListener(new OnCompleteListener<Void>(){
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            Toast.makeText(SignUp.this, "Registartion Successfull", Toast.LENGTH_SHORT).show();
-                                            //TODO
-                                        }
-                                    });
+
+
+
+
+                                } else {
+                                    Toast.makeText(SignUp.this,"SucccessFull",Toast.LENGTH_SHORT).show();
+
+                                    UserHelperClass helperClass = new UserHelperClass(username, email);
+                                    myRef.child(username).setValue(helperClass);
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(SignUp.this, Login.class);
+                                    startActivity(intent);
 
                                 }
                             }
+
+
                         });
+
+
+                UserHelperClass helperClass = new UserHelperClass(username, email);
+                myRef.child(username).setValue(helperClass);
+
 
             }
         });
 
 
+        //Already registerd user back to login Screen
+        regToLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
+
+    private Boolean registerUser() {
+        String val = regUsername.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            regUsername.setError("Field cannot be empty");
+            return false;
+        } else {
+            regUsername.setError(null);
+            regUsername.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean registerEmail() {
+        String val = regEmail.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (val.isEmpty()) {
+            regEmail.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            regEmail.setError("Invalid email address");
+            return false;
+        } else {
+            regEmail.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean registerPassword() {
+        String val = regPassword.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +       //at least 1 digit
+                //"(?=.*[a-z])" +       //at least 1 lower case letter
+                //"(?=.*[A-Z])" +       //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if (val.isEmpty()) {
+            regPassword.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            regPassword.setError("Password is too weak");
+            return false;
+        } else {
+            regPassword.setError(null);
+            return true;
+        }
+    }
+
 }
